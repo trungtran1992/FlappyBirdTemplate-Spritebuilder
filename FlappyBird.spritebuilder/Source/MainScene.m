7a1486728e06a5f61dcbd55f31218eeea6379462
Viet Trung Tran <trung_tran92@Viets-MacBook-Pro.local>
2015-02-26 18:55:07 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -40,7 +40,7 @@
     }
     
     // set this class as delegate
-    physicsNode.collisionDelegate = self;
+    PhysicsNode.collisionDelegate = self;
     
     _obstacles = [NSMutableArray array];
     points = 0;
